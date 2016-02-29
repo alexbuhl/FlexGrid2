c9ee52d7a669cf7daa2366089f158dc0e950a939
Benoist Jean-Baptiste <jean_baptiste.benoist@epita.fr>
2016-02-29 21:32:22 +0100

    amélioration de la voiture, écran scindé et ia eb cours

---
+++ assets/Script/CarCamera.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarCamera : MonoBehaviour {
+
+    public Transform car;
+    public float distance;
+    public float height;
+    public float rotationDamping;
+    public float heightDamping;
+    public float zoomRatio;
+    public float DefaultFOV;
+    private Vector3 rotatioVector;
+	// Use this for initialization
+	void Start () {
+        var wantedAngle = car.eulerAngles.y;
+        var wantedHeight = car.position.y;
+        var myAngle = transform.eulerAngles.y;
+        var myHeight = transform.position.y;
+        myAngle = Mathf.LerpAngle(myAngle, wantedAngle, rotationDamping * Time.deltaTime);
+        myHeight = Mathf.LerpAngle(myHeight, wantedHeight, heightDamping * Time.deltaTime);
+        var currentRotation = Quaternion.Euler(0, myAngle, 0);
+        transform.position = car.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+        transform.position.Set(transform.position.x, myHeight, transform.position.z);
+        transform.LookAt(car);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var localVelocity = car.InverseTransformDirection(car.transform.position);
+        if (localVelocity.z < -0.5)
+        {
+            rotatioVector.y = car.eulerAngles.y + 180;
+        }
+        else
+        {
+            rotatioVector.y = car.eulerAngles.y;
+        }
+        var acc = car.transform.position.magnitude;
+        Camera.main.fieldOfView = DefaultFOV + acc * zoomRatio;
+	}
+}
--- assets/Script/CarControls.cs
+++ assets/Script/CarControls.cs
@@ -1,39 +1,183 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CarControls : MonoBehaviour {
+public class CarControls : MonoBehaviour
+{
 
-	public int speed; // Vitesse de mouvement
-	public int angle; // angle de rotation
-	public KeyCode left; // Touche clavier gauche
-	public KeyCode right; // Touche droite
-	public KeyCode up; // Touche haut
-	public KeyCode backward; // Touche bas
-	public WheelCollider wheelLR;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey (up)) {
-			transform.Translate (Vector3.forward * speed * Time.deltaTime);
-		}
-		else if (Input.GetKey (backward)) {
-			transform.Translate (Vector3.forward *  (- speed / 2) * Time.deltaTime);
-		}
-		//bool a = true;
-		//while (a) {
-		//	a = false;
-			if (Input.GetKey (left)) {
-				this.transform.Rotate (new Vector3 (0, -angle, 0));
-		//		a = true;
-			}
-			if (Input.GetKey (right)) {
-				this.transform.Rotate (new Vector3 (0, angle, 0));
-		//		a = true;
-		//	}
-		}
-	}
+    public int speed; // Vitesse de mouvement
+    public int angle; // angle de rotation
+    public KeyCode left; // Touche clavier gauche
+    public KeyCode right; // Touche droite
+    public KeyCode up; // Touche haut
+    public KeyCode backward; // Touche bas
+    private int reallyspeed;
+    private int reallybackspeed;
+    public Transform body;
+    public Rigidbody rigid;
+    public Transform wheelFRtransf;
+    public Transform wheelRRtransf;
+    public Transform wheelFLtransf;
+    public Transform wheelRLtransf;
+    public Transform etrierFRtransf;
+    public Transform etrierRRtransf;
+    public Transform etrierFLtransf;
+    public Transform etrierRLtransf;
+    public int rotatespeed;
+    public int rotatewheel;
+    // Use this for initialization
+    void Start()
+    {
+        reallyspeed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(up))
+        {
+            rigid.centerOfMass = new Vector3(0, -1, 0);
+            if (reallyspeed < speed)
+            {
+                reallyspeed += 1;
+            }
+            transform.Translate(Vector3.forward * reallyspeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(backward))
+        {
+            rigid.centerOfMass = new Vector3(0, 1, 0);
+            if (reallybackspeed < speed / 2)
+            {
+                reallybackspeed += 1;
+            }
+            transform.Translate(Vector3.forward * (-reallybackspeed) * Time.deltaTime);
+        }
+        if (Input.GetKey(left) && (Input.GetKey(up) || Input.GetKey(backward) || reallybackspeed > 1 || reallyspeed > 1))
+        {
+            this.transform.Rotate(new Vector3(0, -angle, 0));
+        }
+        if (Input.GetKey(right) && (Input.GetKey(up) || Input.GetKey(backward) || reallybackspeed > 1 || reallyspeed > 1))
+        {
+            this.transform.Rotate(new Vector3(0, angle, 0));
+        }
+        if (reallyspeed > 1 & !Input.GetKey(up))
+        {
+            reallyspeed -= 1;
+            transform.Translate(Vector3.forward * reallyspeed * Time.deltaTime);
+        }
+
+        if (reallybackspeed > 1 & !Input.GetKey(backward))
+        {
+            reallybackspeed -= 1;
+            transform.Translate(Vector3.forward * (-reallybackspeed) * Time.deltaTime);
+        }
+        if (Input.GetKey(up))
+        {
+            wheelFLtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelFRtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRRtransf.Rotate(-rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRLtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(backward))
+        {
+            wheelFLtransf.Rotate(- rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelFRtransf.Rotate(- rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRRtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRLtransf.Rotate(-rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(right))
+        {
+            wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y + rotatewheel, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y + rotatewheel, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, etrierFRtransf.localEulerAngles.y + rotatewheel, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, etrierFLtransf.localEulerAngles.y + rotatewheel, 0);
+        }
+        if (Input.GetKey(left))
+        {
+            wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y - rotatewheel, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y - rotatewheel, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, etrierFRtransf.localEulerAngles.y - rotatewheel, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, etrierFLtransf.localEulerAngles.y - rotatewheel, 0);
+        }
+        if (Input.GetKey(right) & Input.GetKey(backward))
+        {
+            wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y + rotatewheel, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y + rotatewheel, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, etrierFRtransf.localEulerAngles.y + rotatewheel, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, etrierFLtransf.localEulerAngles.y + rotatewheel, 0);
+            wheelFLtransf.Rotate(- rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelFRtransf.Rotate(- rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRRtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRLtransf.Rotate(-rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(left) & Input.GetKey(backward))
+        {
+            wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y - rotatewheel, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y - rotatewheel, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, etrierFRtransf.localEulerAngles.y - rotatewheel, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, etrierFLtransf.localEulerAngles.y - rotatewheel, 0);
+            wheelFLtransf.Rotate(- rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelFRtransf.Rotate(- rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRRtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRLtransf.Rotate(-rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(right) & Input.GetKey(up))
+        {
+            wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y + rotatewheel, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y + rotatewheel, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, etrierFRtransf.localEulerAngles.y + rotatewheel, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, etrierFLtransf.localEulerAngles.y + rotatewheel, 0);
+            wheelFLtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelFRtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRRtransf.Rotate(-rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRLtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(left) & Input.GetKey(up))
+        {
+            wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y - rotatewheel, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y - rotatewheel, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, etrierFRtransf.localEulerAngles.y - rotatewheel, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, etrierFLtransf.localEulerAngles.y - rotatewheel, 0);
+            wheelFLtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelFRtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRRtransf.Rotate(-rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+            wheelRLtransf.Rotate(rotatespeed / 60 * 360 * Time.deltaTime, 0, 0);
+        }
+        /*
+        if (!Input.GetKey(right) & !Input.GetKey(left) & wheelFLtransf.localEulerAngles.y != 0)
+        {
+            if (wheelFLtransf.localEulerAngles.y > 0)
+            {
+                wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y - 1, 0);
+                wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y - 1, 0);
+            }
+            if (wheelFLtransf.localEulerAngles.y < 0)
+            {
+                wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y + 1, 0);
+                wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y + 1, 0);
+            }
+        }*/
+        if (Input.GetKeyUp(left) || Input.GetKeyUp(right))
+        {
+            wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y * 3 / 4, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y * 3 / 4, 0);
+            wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y / 2, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y / 2, 0);
+            wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y / 4, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y / 4, 0);
+            wheelFRtransf.localEulerAngles = new Vector3(0, wheelFRtransf.localEulerAngles.y / 8, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, wheelFLtransf.localEulerAngles.y / 8, 0);
+            wheelFRtransf.localEulerAngles = new Vector3(0, 0, 0);
+            wheelFLtransf.localEulerAngles = new Vector3(0, 0, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, etrierFRtransf.localEulerAngles.y * 3 / 4, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, etrierFLtransf.localEulerAngles.y * 3 / 4, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, etrierFRtransf.localEulerAngles.y / 2, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, etrierFLtransf.localEulerAngles.y / 2, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, etrierFRtransf.localEulerAngles.y / 4, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, etrierFLtransf.localEulerAngles.y / 4, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, etrierFRtransf.localEulerAngles.y / 8, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, etrierFLtransf.localEulerAngles.y / 8, 0);
+            etrierFRtransf.localEulerAngles = new Vector3(0, 0, 0);
+            etrierFLtransf.localEulerAngles = new Vector3(0, 0, 0);
+        }
+    }
 }

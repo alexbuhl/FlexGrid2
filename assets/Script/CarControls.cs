50ae1c3f3a1558a9348f79a43e2ce3a27ac4d982
Benoist Jean-Baptiste <jean_baptiste.benoist@epita.fr>
2016-03-02 16:21:17 +0100

    ajout de la force centrifuge, frein à main en cours

--- assets/Script/CarControls.cs
+++ assets/Script/CarControls.cs
@@ -10,6 +10,7 @@ public class CarControls : MonoBehaviour
     public KeyCode right; // Touche droite
     public KeyCode up; // Touche haut
     public KeyCode backward; // Touche bas
+    public KeyCode derapage;
     private int reallyspeed;
     private int reallybackspeed;
     public Transform body;
@@ -29,6 +30,12 @@ public class CarControls : MonoBehaviour
     public int rotatespeed;
     public int rotatewheel;
     public int enfoncement;
+    public int forcecentrifuge;
+    public int angledérapage;
+    public int decalcamdera;
+    public int rotcamdera;
+    public Transform car;
+    public Camera camera;
     // Use this for initialization
     void Start()
     {
@@ -109,13 +116,13 @@ public class CarControls : MonoBehaviour
             rigid.centerOfMass = new Vector3(0, 0, 0);
             
         }
-        if (Input.GetKey(left) && (Input.GetKey(up) || Input.GetKey(backward) || reallybackspeed > 1 || reallyspeed > 1))
+        if (Input.GetKey(left) && (Input.GetKey(up) || Input.GetKey(backward) || reallybackspeed > 1 || reallyspeed > 1) & !(Input.GetKey(derapage)))
         {
-            this.transform.Rotate(new Vector3(0, -angle, 0));
+            this.transform.Rotate(new Vector3(0, -angle + reallyspeed / forcecentrifuge, 0));
         }
-        if (Input.GetKey(right) && (Input.GetKey(up) || Input.GetKey(backward) || reallybackspeed > 1 || reallyspeed > 1))
+        if (Input.GetKey(right) && (Input.GetKey(up) || Input.GetKey(backward) || reallybackspeed > 1 || reallyspeed > 1) & !(Input.GetKey(derapage)))
         {
-            this.transform.Rotate(new Vector3(0, angle, 0));
+            this.transform.Rotate(new Vector3(0, angle - reallyspeed / forcecentrifuge, 0));
         }
         if (reallyspeed > 1 & !Input.GetKey(up))
         {
@@ -145,6 +152,30 @@ public class CarControls : MonoBehaviour
                 ralentie.Stop();
             }
             décélération.Play();
+        }
+        if (Input.GetKey(derapage) & Input.GetKey(left) & Input.GetKey(up))
+        {
+            camera.transform.Translate(Vector3.left * decalcamdera * Time.deltaTime);
+            camera.transform.Rotate(- rotcamdera / 60 * 360 * Time.deltaTime, 0, 0);
+            this.transform.Rotate(new Vector3(0, - angledérapage, 0));
+            if (reallyspeed > 4)
+            {
+                reallyspeed -= 3;
+            }
+        }
+        if (Input.GetKey(derapage) & Input.GetKey(right) & Input.GetKey(up))
+        {
+            camera.transform.Translate(Vector3.right * decalcamdera * Time.deltaTime);
+            camera.transform.Rotate(rotcamdera / 60 * 360 * Time.deltaTime, 0, 0);
+            this.transform.Rotate(new Vector3(0, angledérapage, 0));
+            if (reallyspeed > 4)
+            {
+                reallyspeed -= 3;
+            }
+        }
+        if (!Input.GetKeyUp(derapage))
+        {
+            car.Rotate(new Vector3 (0,0,0));
         }
         if (Input.GetKey(up))
         {
